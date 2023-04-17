9ad64dc8ab0ecc519d471abbe61e77ab431f6f53
d.lutsenko <d.lutsenko@tinkoff.ru>
2023-04-17 18:35:12 +0300

    imports remove

--- src/main/java/com/hello/hello1/HelloController.java
+++ src/main/java/com/hello/hello1/HelloController.java
@@ -1,11 +1,8 @@
 package com.hello.hello1;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.test.starter.config.StarterMyConfiguration;
 import ru.test.starter.service.MyService;
 
 @RestController

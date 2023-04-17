aa1ba1672824c42ded75f785e14056d649dcaefd
d.lutsenko <d.lutsenko@tinkoff.ru>
2023-04-17 18:34:48 +0300

    a1

--- src/main/java/com/hello/hello1/Hello1Application.java
+++ src/main/java/com/hello/hello1/Hello1Application.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import ru.test.starter.StarterApplication;
+import ru.test.starter.config.StarterMyConfiguration;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@Import(value = {StarterMyConfiguration.class})
 public class Hello1Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/hello/hello1/HelloController.java
+++ src/main/java/com/hello/hello1/HelloController.java
@@ -1,6 +1,7 @@
 package com.hello.hello1;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,23 +11,12 @@ import ru.test.starter.service.MyService;
 @RestController
 public class HelloController {
 
-    public static final String MY_SERVICE_BEAN_NAME = "myService";
-    MyService myService = null;
-
-    @PostConstruct
-    public void init() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(StarterMyConfiguration.class);
-        if (context.containsBean(MY_SERVICE_BEAN_NAME)) {
-            myService = (MyService) context.getBean("myService");
-        }
-    }
+    @Autowired
+    MyService myService;
 
 
     @GetMapping("/hello")
     public String hello() {
-        if (null != myService) {
-            return myService.doSomething();
-        }
-        return "Hello";
+        return myService.doSomething();
     }
 }

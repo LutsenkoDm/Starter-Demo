90c33de56b6ab9551134d73a8e282414fbe4260e
d.lutsenko <d.lutsenko@tinkoff.ru>
2023-04-18 13:10:05 +0300

    fix1

--- src/main/java/com/hello/hello1/Hello1Application.java
+++ src/main/java/com/hello/hello1/Hello1Application.java
@@ -1,24 +1,13 @@
 package com.hello.hello1;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import ru.test.starter.StarterApplication;
-import ru.test.starter.config.StarterMyConfiguration;
-
-import java.util.Arrays;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@Import(value = {StarterMyConfiguration.class})
 public class Hello1Application {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext run = SpringApplication.run(Hello1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Hello1Application.class, args);
+    }
 
 }
--- src/main/java/com/hello/hello1/HelloController.java
+++ src/main/java/com/hello/hello1/HelloController.java
@@ -1,19 +1,15 @@
 package com.hello.hello1;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.test.starter.service.MyService;
 
 @RestController
 public class HelloController {
 
-    @Autowired
-    MyService myService;
-
-
-    @GetMapping("/hello")
-    public String hello() {
-        return myService.doSomething();
+    @RequestMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok()
+                .body("Hello");
     }
 }

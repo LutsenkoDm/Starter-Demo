2cd22986fce195d15d69e21dbf012d157201c4a8
d.lutsenko <d.lutsenko@tinkoff.ru>
2023-04-18 13:13:05 +0300

    fix2

--- src/main/java/com/hello/hello1/HelloConfig.java
+++ src/main/java/com/hello/hello1/HelloConfig.java
@@ -13,6 +13,9 @@ public class HelloConfig implements WebMvcConfigurer {
     public static final String HEADER_INTERCEPTOR_NAME = "headerInterceptor";
     public static final String STARTER_CONFIG_CLASS_NAME = "ru.test.starter.config.StarterMyConfiguration";
 
+    // Кажется это стоило бы писать в интерцепторе, я так делал но тогда его не видно
+    // Разве что сделать такой WebMvcConfigurer в интерцепотре и явно добавлять в текущий контекст
+    // если вообще так можно
     @Override
     @ConditionalOnClass(name = STARTER_CONFIG_CLASS_NAME)
     public void addInterceptors(InterceptorRegistry registry) {
